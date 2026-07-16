a6fae8b8f249121ce07265a14f7d90a2e8ec707b
Muhammad Anurohim <muhammadanurohim23@gmail.com>
2026-07-16 09:17:18 +0700

    chore: remove test file

--- test_fillable.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
-
-$user = \App\Models\User::first();
-var_dump($user->getFillable());
